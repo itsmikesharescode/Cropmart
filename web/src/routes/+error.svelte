a79ce8d6737b193308849121615762e0e9938e9f
itsmikesharescode <mikejohneviota@gmail.com>
2024-09-29 23:12:12 +0800

    add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex min-h-screen flex-col items-center justify-center gap-2.5">
+	<p class="text-muted-foreground">The page you are looking for is not in our index.</p>
+	<a href="/" class="text-sm transition-all hover:scale-95 hover:bg-primary">Back to home</a>
+</div>

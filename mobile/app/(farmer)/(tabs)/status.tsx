4d3d8fe587f147afd82d08aa5a93340654cd313a
itsmikesharescode <mikejohneviota@gmail.com>
2024-10-03 23:30:51 +0800

    add more details

--- mobile/app/(farmer)/(tabs)/status.tsx
+++ mobile/app/(farmer)/(tabs)/status.tsx
@@ -25,6 +25,13 @@ const StatusSnippet: React.FC<EntrepLayoutQ['processings'][number]> = (transacti
         </Text>
       </View>
 
+      <View className="flex-row gap-[10px] flex-wrap absolute bottom-[82px] left-3">
+        <Text className="font-psemibold text-[15px] px-5 bg-secondary/80">
+          Created At: {new Date(transaction.created_at).toLocaleDateString()} @{' '}
+          {new Date(transaction.created_at).toLocaleTimeString()}
+        </Text>
+      </View>
+
       <View className="flex-row gap-[10px] flex-wrap absolute bottom-3 left-3">
         <Text className="font-psemibold text-[15px] px-5 bg-secondary/80">
           Price: ₱ {transaction.product_obj.price.toLocaleString()}

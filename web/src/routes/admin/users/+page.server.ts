0592cba8f42a03885a8db20c70b7ae2f056792ca
itsmikesharescode <mikejohnb@gmail.com>
2024-10-05 19:23:12 +0800

    update user server actions

--- web/src/routes/admin/users/+page.server.ts
+++ web/src/routes/admin/users/+page.server.ts
@@ -1,5 +1,5 @@
 import { superValidate } from 'sveltekit-superforms';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import {
   createUserSchema,
@@ -7,6 +7,7 @@ import {
   updateUserInfoSchema,
   updateUserPwdSchema
 } from './_components/operations/schema';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
   return {
@@ -15,4 +16,38 @@ export const load: PageServerLoad = async () => {
     updateUserEmailForm: await superValidate(zod(updateUserEmailSchema)),
     updateUserPwdForm: await superValidate(zod(updateUserPwdSchema))
   };
+};
+
+export const actions: Actions = {
+  createUserEvent: async ({ locals: { supabase }, request }) => {
+    const form = await superValidate(request, zod(createUserSchema));
+
+    if (!form.valid) return fail(400, { form });
+
+    console.log(form.data);
+  },
+
+  updateUserInfoEvent: async ({ locals: { supabase }, request }) => {
+    const form = await superValidate(request, zod(updateUserInfoSchema));
+
+    if (!form.valid) return fail(400, { form });
+
+    console.log(form.data);
+  },
+
+  updateUserEmailEvent: async ({ locals: { supabase }, request }) => {
+    const form = await superValidate(request, zod(updateUserEmailSchema));
+
+    if (!form.valid) return fail(400, { form });
+
+    console.log(form.data);
+  },
+
+  updateUserPwdEvent: async ({ locals: { supabase }, request }) => {
+    const form = await superValidate(request, zod(updateUserPwdSchema));
+
+    if (!form.valid) return fail(400, { form });
+
+    console.log(form.data);
+  }
 };

283e1cb0c968347cd729d96c9eab1b74952905cf
itsmikesharescode <mikejohnb@gmail.com>
2024-10-06 19:41:49 +0800

    update dashboard

--- web/src/routes/admin/(index)/+page.svelte
+++ web/src/routes/admin/(index)/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+  import { fromCategoryState } from '../_route_states/catRoute.svelte';
+  import { fromProductState } from '../_route_states/prodRoute.svelte';
+  import { fromUserManagementState } from '../_route_states/userRoute.svelte';
   import BarChart from './_components/BarChart.svelte';
   import LineChart from './_components/LineChart.svelte';
+
+  const productState = fromProductState();
+  const categoryState = fromCategoryState();
+  const userManagementState = fromUserManagementState();
 </script>
 
-{#snippet CounterCard()}
+{#snippet CounterCard(title: string, count: number)}
   <div class="max-w-[400px] rounded-lg border-2 border-primary p-4">
-    <p class="text-center text-[2rem] font-bold text-primary">Products</p>
-    <p class="text-center text-[3rem] font-bold text-primary/50">20</p>
+    <p class="text-center text-[2rem] font-bold text-primary">{title}</p>
+    <p class="text-center text-[3rem] font-bold text-primary/50">{count}</p>
   </div>
 {/snippet}
 
@@ -21,8 +28,13 @@
   </div>
 
   <div class="grid grid-cols-3 gap-2.5">
-    {@render CounterCard()}
-    {@render CounterCard()}
-    {@render CounterCard()}
+    {@render CounterCard('Products', productState.getProucts()?.length ?? 0)}
+    {@render CounterCard('Categories', categoryState.getCategories()?.length ?? 0)}
+    {@render CounterCard(
+      'Users',
+      (userManagementState.getFarmers()?.length ?? 0) +
+        (userManagementState.getEntrepreneurs()?.length ?? 0) +
+        (userManagementState.getRiders()?.length ?? 0)
+    )}
   </div>
 </div>

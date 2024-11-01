5fc5f5132e6c996c8fac2d7d178fcb462db1166c
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-01 21:52:45 +0800

    update sql

--- web/src/routes/admin/+layout.server.ts
+++ web/src/routes/admin/+layout.server.ts
@@ -3,6 +3,10 @@ import type { LayoutServerLoad } from './$types';
 import type { AdminLayoutQ } from '$lib/types';
 
 export const load: LayoutServerLoad = async ({ locals: { supabase } }) => {
+  const { data, error } = await supabase.rpc('get_monthly_status_counts');
+
+  console.log(data, error?.message);
+
   return {
     adminLayoutQ: (await supabase.rpc('admin_layout_q')) as PostgrestSingleResponse<AdminLayoutQ>
   };
